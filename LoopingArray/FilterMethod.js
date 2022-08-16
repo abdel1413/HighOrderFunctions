f81e1f42e3137a47c251de4c2a534066430a04ff
Your Name <you@example.com>
2022-08-16 02:53:46 -0400

    added function map

--- LoopingArray/FilterMethod.js
+++ LoopingArray/FilterMethod.js
@@ -5,7 +5,7 @@ import scripts from "../SCRIPTS";
  * @param {*} array
  * @param {*} test
  * filter methd filter out element that do not pass the test
- * and out print those who passed the test
+ * and build up a new array with only those who passed the test
  */
 function filtering(array, test) {
   let filteredArray = [];
@@ -25,3 +25,12 @@ console.log(filtering(scripts, (script) => script.direction));
 
 console.log(filtering(scripts, (script) => script.year === "1599"));
 //filter only the array containing the year 1599
+
+//NOTE WE CAN USE "filter array standart method " for array when
+//looking for specific elements
+
+console.log(scripts.filter((script) => script.direction === "ttb"));
+// // => 3 [{…}, {…}, {…}]
+//// =>  {name: 'Mongolian', ranges: Array(7), direction: 'ttb', year: 1204, living: false, …}
+// => 1: {name: 'Phags-pa', ranges: Array(2), direction: 'ttb', year: 1269, living: false, …}
+// => 2: {name: 'SignWriting', ranges: Array(3), direction: 'ttb', year: 1974, living: true, …}
---
+++ LoopingArray/Mapping.js
@@ -0,0 +1,27 @@
+import SCRIPTS from "../SCRIPTS";
+
+/**
+ *
+ * @param {*} array
+ * @param {*} transforms
+ * NOTE: map method transforms an object array or array
+ *  to new array from returned values by applying function
+ * to all the object array elements.
+ * the new array has the same length as the original
+ */
+
+function mapping(array, transforms) {
+  let mappedElement = [];
+  for (let element of array) {
+    // let transformedElement = transforms(element)
+    // mappedElement.push(transformedElement)
+    mappedElement.push(transforms(element));
+  }
+  return mappedElement;
+}
+
+let rtlScripts = SCRIPTS.filter((sc) => sc.direction === "rtl");
+console.log(mapping(rtlScripts, (script) => script.name));
+// => (139) ['Adlam', 'Caucasian Albanian', 'Ahom', 'Arabic',...]
+
+//the standart array method is: array.map(param => param.something)
