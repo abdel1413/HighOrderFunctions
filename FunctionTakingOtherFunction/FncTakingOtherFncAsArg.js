dd05fb65076d701192426985847af2d2bb203184
Your Name <you@example.com>
2022-08-28 08:31:57 -0400

    added fucntin

--- FunctionTakingOtherFunction/FncTakingOtherFncAsArg.js
+++ FunctionTakingOtherFunction/FncTakingOtherFncAsArg.js
@@ -1,12 +1,20 @@
-function firstFunction(x, action) {
-  return action(x); //this implicitly the functoin called otherFuntoin
+function firstFunction(x, action, conditionFnc) {
+  if (conditionFnc(x)) {
+    return action(x); //this implicitly the functoin called otherFuntoin
+  } else {
+    return `${x} is not less than ...`;
+  }
+}
+
+function conditionChecker(x) {
+  return x < 5;
 }
 
 function otherFunction(x) {
   return -x;
 }
 
-console.log(firstFunction(5, otherFunction)); // => -5
+console.log(firstFunction(5, otherFunction, conditionChecker)); // => -5
 
 function combine(num, InsideCombine) {
   let tempo;
@@ -18,7 +26,7 @@ function combine(num, InsideCombine) {
 
 function functionInsideCombine(x) {
   return x + 2;
-
+  //0 + 2 = 2
   // 1 + 2,  =3
   // 2 + 2 = 4
   // 3+2 = 5
